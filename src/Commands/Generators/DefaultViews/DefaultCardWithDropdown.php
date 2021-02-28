f4b226c18c72928f3745a2c7bda117ca65ba7ca3
Ralf Kornberger <37985317+rakoitde@users.noreply.github.com>
2021-02-28 19:39:49 +0100

    28.02.2021

---
+++ src/Commands/Generators/DefaultViews/DefaultCardWithDropdown.php
@@ -0,0 +1,20 @@
+<!-- Start: Card with Dropdows -->
+<div class="card">
+  <div class="card-header d-flex justify-content-between">
+    Featured
+<div class="btn-group">
+  <button type="button" class="btn btn-sm  py-0 px-2 dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></button>
+  <div class="dropdown-menu dropdown-menu-right" style="">
+    <button class="dropdown-item" type="button">Action</button>
+    <button class="dropdown-item" type="button">Another action</button>
+    <button class="dropdown-item" type="button">Something else here</button>
+  </div>
+</div>
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+<!-- End: Card with Dropdows -->
