25186991e84f3dba2ce264b93e590d68636ec900
Ralf Kornberger <37985317+rakoitde@users.noreply.github.com>
2022-10-01 16:58:25 +0200

    Update ToolsPublish.php

--- src/Commands/ToolsPublish.php
+++ src/Commands/ToolsPublish.php
@@ -12,21 +12,26 @@ class ToolsPublish extends BaseCommand
     protected $name        = 'Tools:publish';
     protected $description = 'Publish Tools components into the current application.';
 
+    protected $source;
+
     public function run(array $params)
     {
-        // Use the Autoloader to figure out the module path
-        $source = service('autoloader')->getNamespace('Rakoitde\\Tools');
 
-        $publisher = new Publisher($source[0], APPPATH);
+        $this->source = service('autoloader')->getNamespace('Rakoitde\\Tools')[0];
+
+        $this->publishCommands();
+
+    }
+
+    private function publish()
+    {
+
+        $publisher = new Publisher($this->source, APPPATH);
 
         try {
-            // Add only the desired components
-            $publisher->addPaths([
-                'Commands/*Command.php',
-            ])->merge(false); // Be careful not to overwrite anything
+            $publisher->addPaths(['Commands/*Command.php','Entities/*.php'])->merge(false); // Be careful not to overwrite anything
         } catch (Throwable $e) {
             $this->showError($e);
-
             return;
         }
 
@@ -37,5 +42,29 @@ class ToolsPublish extends BaseCommand
             $contents = str_replace('namespace Rakoitde\\Tools', 'namespace ' . APP_NAMESPACE, $contents);
             file_put_contents($file, $contents);
         }
+
     }
+
+    private function ____publishEntityRelationTrait()
+    {
+
+        $publisher = new Publisher($this->source, APPPATH);
+
+        try {
+            $publisher->addPaths(['Entities/*.php'])->merge(false); // Be careful not to overwrite anything
+        } catch (Throwable $e) {
+            $this->showError($e);
+            return;
+        }
+
+        // If publication succeeded then update namespaces
+        foreach ($publisher->getPublished() as $file) {
+            // Replace the namespace
+            $contents = file_get_contents($file);
+            $contents = str_replace('namespace Rakoitde\\Tools', 'namespace ' . APP_NAMESPACE, $contents);
+            file_put_contents($file, $contents);
+        }
+
+    }
+
 }
