6a991fd137bb641ee9ba39cef2cde21370c33fac
Ralf Kornberger <37985317+rakoitde@users.noreply.github.com>
2022-11-20 21:02:05 +0100

    Update Services.php

--- src/Config/Services.php
+++ src/Config/Services.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * This file is part of CodeIgniter 4 Tools.
+ *
+ * (c) 2022 Ralf Kornberger <rakoitde@gmail.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
 namespace Rakoitde\Tools\Config;
 
 use CodeIgniter\Config\BaseService;
