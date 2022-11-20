9a4a0d6627300ff9a8b449ec8ca7aac51546931e
Ralf Kornberger <37985317+rakoitde@users.noreply.github.com>
2022-11-20 21:02:45 +0100

    Update Constraint.php

--- src/Constraint.php
+++ src/Constraint.php
@@ -11,53 +11,38 @@
 
 namespace Rakoitde\Tools;
 
-use Rakoitde\Tools\Config\Tools;
-
-use Rakoitde\Tools\Command;
-
 /**
  * This class describes a Constraint.
  */
-
 class Constraint
 {
-
     public $name;
-
     public $table_name;
-
     public $column_name;
-
     public $referenced_table_name;
-
     public $referenced_column_name;
-
     public $delete_rule;
-
     public $update_rule;
 
     public function fill($constraint)
     {
-        $this->name                   = $constraint["CONSTRAINT_NAME"];
-        $this->table_name             = $constraint["TABLE_NAME"];
-        $this->column_name            = $constraint["COLUMN_NAME"];
-        $this->referenced_table_name  = $constraint["REFERENCED_TABLE_NAME"];
-        $this->referenced_column_name = $constraint["REFERENCED_COLUMN_NAME"];
-        $this->delete_rule            = $constraint["DELETE_RULE"];
-        $this->update_rule            = $constraint["UPDATE_RULE"];
+        $this->name                   = $constraint['CONSTRAINT_NAME'];
+        $this->table_name             = $constraint['TABLE_NAME'];
+        $this->column_name            = $constraint['COLUMN_NAME'];
+        $this->referenced_table_name  = $constraint['REFERENCED_TABLE_NAME'];
+        $this->referenced_column_name = $constraint['REFERENCED_COLUMN_NAME'];
+        $this->delete_rule            = $constraint['DELETE_RULE'];
+        $this->update_rule            = $constraint['UPDATE_RULE'];
     }
-
 
     public function getDropCommand()
     {
-        $command = "ALTER TABLE `{$this->table_name}` DROP FOREIGN KEY `{$this->name}`;";
-        return $command;
+        return "ALTER TABLE `{$this->table_name}` DROP FOREIGN KEY `{$this->name}`;";
     }
 
     public function getAddCommand()
     {
-        $command = "ALTER TABLE `{$this->table_name}` ADD CONSTRAINT `{$this->name}` FOREIGN KEY (`{$this->column_name}`) REFERENCES `{$this->referenced_table_name}` (`{$this->referenced_column_name}`) ON DELETE {$this->delete_rule} ON UPDATE {$this->update_rule};";
-        return $command;
+        return "ALTER TABLE `{$this->table_name}` ADD CONSTRAINT `{$this->name}` FOREIGN KEY (`{$this->column_name}`) REFERENCES `{$this->referenced_table_name}` (`{$this->referenced_column_name}`) ON DELETE {$this->delete_rule} ON UPDATE {$this->update_rule};";
     }
 
     public function __toString()
@@ -69,5 +54,4 @@ class Constraint
     {
         $this->fill($constraint);
     }
-
 }
