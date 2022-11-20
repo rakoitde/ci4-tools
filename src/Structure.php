ccdf961985915e91f8497953958d685cf84b3e43
Ralf Kornberger <37985317+rakoitde@users.noreply.github.com>
2022-11-20 21:03:51 +0100

    Update Structure.php

--- src/Structure.php
+++ src/Structure.php
@@ -13,35 +13,22 @@ namespace Rakoitde\Tools;
 
 use Rakoitde\Tools\Config\Tools;
 
-use Rakoitde\Tools\Command;
-
 /**
  * This class describes database tools.
  */
 class Structure
 {
-
     public string $dbgroup;
-
     public string $table;
-
     protected $CHARACTER_SET = 'utf8 COLLATE utf8_general_ci';
-
     protected $from;
-
     protected $to;
-
     public array $from_commands = [];
-
-    public array $to_commands = [];
-
-    public $fields_to_update = [];
-
-    public $fields_to_add = [];
-
-    public $fields_to_drop = [];
-
-    public $fields_equal = [];
+    public array $to_commands   = [];
+    public $fields_to_update    = [];
+    public $fields_to_add       = [];
+    public $fields_to_drop      = [];
+    public $fields_equal        = [];
 
     /**
      * { function_description }
@@ -51,9 +38,7 @@ class Structure
     public function from($from)
     {
         foreach ($from as $field) {
-            
             $this->from[$field['Field']] = $field;
-            
         }
     }
 
@@ -65,9 +50,7 @@ class Structure
     public function to($to)
     {
         foreach ($to as $field) {
-            
             $this->to[$field['Field']] = $field;
-            
         }
     }
 
@@ -76,53 +59,39 @@ class Structure
      */
     public function compare()
     {
-
-        $to = $this->to;
+        $to   = $this->to;
         $from = $this->from;
 
         foreach ($from as $key => $field) {
-                 
-            if (!isset($to[$key])) {
-
+            if (! isset($to[$key])) {
                 $this->fields_to_add[] = $key;
-
-            } elseif ($field!==$to[$key]) {
-
+            } elseif ($field !== $to[$key]) {
                 $this->fields_to_update[] = $key;
-
             } else {
                 $this->fields_equal[] = $key;
             }
-            
         }
 
         $this->fieldsToUpdate();
         $this->fieldsToAdd();
 
         foreach ($to as $key => $field) {
-                 
-            if (!isset($from[$key])) {
-
+            if (! isset($from[$key])) {
                 $this->fields_to_drop[] = $key;
-
-            } 
-            
+            }
         }
 
         $this->fieldsToDrop();
-
     }
-
 
     public function fieldsToUpdate()
     {
         foreach ($this->fields_to_update as $field) {
-
             $differences = array_diff($this->from[$field], $this->to[$field]);
 
             if (is_array($differences) && ! empty($differences)) {
-                                    // ALTER TABLE `bugs` MODIFY COLUMN `site_name`  varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER `type`;
-                                    // ALTER TABLE `bugs` MODIFY COLUMN `message`  varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER `site_name`;
+                // ALTER TABLE `bugs` MODIFY COLUMN `site_name`  varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER `type`;
+                // ALTER TABLE `bugs` MODIFY COLUMN `message`  varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER `site_name`;
                 $modify_field = "ALTER TABLE {$this->table} CHANGE `" . $this->from[$field]['Field'] . '` `' . $this->from[$field]['Field'] . '` ' . $this->from[$field]['Type'];
                 if (substr($this->from[$field]['Type'], 0, 7) === 'varchar') {
                     $modify_field .= ' CHARACTER SET ' . $this->CHARACTER_SET;
@@ -134,24 +103,21 @@ class Structure
                 $modify_field .= ';';
 
                 if (! in_array($modify_field, $this->to_commands, true)) {
-
-                    $command = new Command();
+                    $command          = new Command();
                     $command->action  = 'change column';
                     $command->dbgroup = '';
                     $command->table   = $this->table;
-                    $command->query   = "";
-                    $command->result  = "";
-                    $command->type    = "field";
+                    $command->query   = '';
+                    $command->result  = '';
+                    $command->type    = 'field';
                     $command->command = $modify_field;
 
                     $this->to_commands[] = $command;
                 }
             }
             $previous_field = $this->from[$field]['Field'];
-
         }
     }
-
 
     public function fieldsToAdd()
     {
@@ -170,14 +136,13 @@ class Structure
             $add_field .= ';';
 
             if (! in_array($add_field, $this->to_commands, true)) {
-
-                $command = new Command();
+                $command          = new Command();
                 $command->action  = 'add column';
                 $command->dbgroup = '';
                 $command->table   = $this->table;
-                $command->query   = "";
-                $command->result  = "";
-                $command->type    = "field";
+                $command->query   = '';
+                $command->result  = '';
+                $command->type    = 'field';
                 $command->command = $add_field;
 
                 $this->to_commands[] = $command;
@@ -185,22 +150,20 @@ class Structure
         }
     }
 
-    
-    # ALTER TABLE tbl_Country DROP COLUMN IsDeleted;
+    // ALTER TABLE tbl_Country DROP COLUMN IsDeleted;
     public function fieldsToDrop()
     {
         foreach ($this->fields_to_drop as $field) {
             $drop_field = "ALTER TABLE {$this->table} DROP COLUMN `" . $this->to[$field]['Field'] . '`;';
 
             if (! in_array($drop_field, $this->to_commands, true)) {
-
-                $command = new Command();
+                $command          = new Command();
                 $command->action  = 'drop column';
                 $command->dbgroup = '';
                 $command->table   = $this->table;
-                $command->query   = "";
-                $command->result  = "";
-                $command->type    = "field";
+                $command->query   = '';
+                $command->result  = '';
+                $command->type    = 'field';
                 $command->command = $drop_field;
 
                 $this->to_commands[] = $command;
@@ -211,11 +174,11 @@ class Structure
     /**
      * Recursive version of in_array
      *
-     * @param      string  $needle
-     * @param      array   $haystack
-     * @param      bool    $strict
+     * @param string $needle
+     * @param array  $haystack
+     * @param bool   $strict
      *
-     * @return     bool
+     * @return bool
      */
     public function inArrayRecursive($needle, $haystack, $strict = false)
     {
@@ -238,5 +201,4 @@ class Structure
     {
         $this->table = $table;
     }
-
 }
