31a292440566242f780688e24f1a86721e060d7d
Kornberger, Ralf <Ralf.Kornberger@kk-service.de>
2026-03-26 17:22:31 +0100

    ref: remove routes

--- src/Config/Routes.php
+++ src/Config/Routes.php
@@ -8,18 +8,3 @@
  * For the full copyright and license information, please view
  * the LICENSE file that was distributed with this source code.
  */
-
-helper('auth');
-
-$routes->group('tools', ['namespace' => 'Rakoitde\Tools\Controllers'], static function ($routes) {
-    $routes->get('database', 'DatabaseController::index');
-    $routes->add('comparedb', 'DatabaseCompare::index');
-    $routes->add('backupdb', 'DatabaseBackup::index');
-    $routes->add('db/(:segment)/backup', 'DatabaseBackup::backup/$1');
-});
-
-$routes->group('api/tools', ['namespace' => 'Rakoitde\Tools\Controllers'], static function ($routes) {
-    $routes->get('environment', 'ApiDatabaseController::environment');
-    $routes->get('tables', 'ApiDatabaseController::tables');
-    $routes->get('compare/(:any)', 'ApiDatabaseController::compare/$1');
-});
